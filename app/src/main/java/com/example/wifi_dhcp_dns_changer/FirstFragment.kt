04ebbb250c891f8f9c999bbb4d8b381c2c28a905
rutilicus <rutilicus.growup@gmail.com>
2022-11-27 01:11:03 +0900

    とりあえずstatic→DHCPへの変更を作ってみたけど動かない、Android MからWifiConfigurationの変更が自己で作成したインスタンスでないとできないらしい

--- app/src/main/java/com/example/wifi_dhcp_dns_changer/FirstFragment.kt
+++ app/src/main/java/com/example/wifi_dhcp_dns_changer/FirstFragment.kt
@@ -3,20 +3,15 @@ package com.example.wifi_dhcp_dns_changer
 import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.Context
 import android.content.pm.PackageManager
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.net.NetworkInfo
-import android.net.wifi.WifiInfo
+import android.net.wifi.WifiConfiguration
 import android.net.wifi.WifiManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
-import androidx.core.app.ActivityCompat
-import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.example.wifi_dhcp_dns_changer.databinding.FragmentFirstBinding
 import java.util.*
 
@@ -29,10 +24,12 @@ class FirstFragment : Fragment() {
 
     private lateinit var textViewSsid: TextView
     private lateinit var textViewIpSetting: TextView
+    private lateinit var textViewIpAddress: TextView
     private lateinit var textViewGateway: TextView
     private lateinit var textViewNetworkPrefixLength: TextView
     private lateinit var textViewDns1: TextView
     private lateinit var textViewDns2: TextView
+    private lateinit var buttonSettingChange: Button
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -42,12 +39,46 @@ class FirstFragment : Fragment() {
         var ret = (ip and 0xFF).toString()
 
         for (i in 1..3) {
-            ret = (ip.ushr(i * 8) and 0xFF).toString() + "." + ret
+            ret += ".${(ip.ushr(i * 8) and 0xFF)}"
         }
 
         return ret
     }
 
+    private fun getEnumValue(className: String, value: String): Any {
+        val enumConstants = Class.forName(className).enumConstants as Array<Enum<*>>
+        return enumConstants.first { it.name == value }
+    }
+
+    private fun callMethod(
+        classObject: Any, methodName: String, parameterTypes: Array<String>, parameterValues: Array<Any>) {
+        val paramClasses = Array(parameterTypes.size) {
+            Class.forName(parameterTypes[it])
+        }
+        val method = classObject.javaClass.getDeclaredMethod(methodName, *paramClasses)
+        method.invoke(classObject, *parameterValues)
+    }
+
+    private fun copyWifiConf(src: WifiConfiguration, dst: WifiConfiguration) {
+        dst.BSSID = src.BSSID
+        dst.FQDN = src.FQDN
+        dst.SSID = dst.SSID
+        dst.allowedAuthAlgorithms = src.allowedAuthAlgorithms
+        dst.allowedGroupCiphers = src.allowedGroupCiphers
+        dst.allowedKeyManagement = src.allowedKeyManagement
+        dst.allowedPairwiseCiphers = src.allowedPairwiseCiphers
+        dst.allowedProtocols = src.allowedProtocols
+        dst.enterpriseConfig = src.enterpriseConfig
+        dst.hiddenSSID = src.hiddenSSID
+        dst.networkId = src.networkId
+        dst.preSharedKey = src.preSharedKey
+        dst.priority = src.priority
+        dst.providerFriendlyName = src.providerFriendlyName
+        dst.roamingConsortiumIds = src.roamingConsortiumIds
+        dst.status = src.status
+        dst.wepKeys = src.wepKeys
+        dst.wepTxKeyIndex = src.wepTxKeyIndex
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -59,20 +90,14 @@ class FirstFragment : Fragment() {
         return binding.root.apply {
             textViewSsid = this.findViewById(R.id.textview_ssid)
             textViewIpSetting = this.findViewById(R.id.textview_ip_setting)
+            textViewIpAddress = this.findViewById(R.id.textview_ip_address)
             textViewGateway = this.findViewById(R.id.textview_gateway)
             textViewNetworkPrefixLength = this.findViewById(R.id.textview_network_prefix_length)
             textViewDns1 = this.findViewById(R.id.textview_dns_1)
             textViewDns2 = this.findViewById(R.id.textview_dns_2)
+            buttonSettingChange = this.findViewById(R.id.button_setting_change)
         }
 
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
     override fun onResume() {
@@ -90,6 +115,7 @@ class FirstFragment : Fragment() {
                                     Locale.getDefault()).indexOf("DHCP".lowercase(Locale.getDefault())) > -1) {
                                 textViewIpSetting.setText(R.string.config_dhcp)
                                 wifiManager.dhcpInfo.apply {
+                                    textViewIpAddress.text = ip2String(ipAddress)
                                     textViewGateway.text = ip2String(gateway)
                                     textViewNetworkPrefixLength.text = netmask.countOneBits().toString()
                                     textViewDns1.text = ip2String(dns1)
@@ -98,9 +124,30 @@ class FirstFragment : Fragment() {
                             } else {
                                 textViewIpSetting.setText(R.string.config_static)
                                 textViewGateway.text = ""
+                                textViewIpAddress.text = ""
                                 textViewNetworkPrefixLength.text = ""
                                 textViewDns1.text = ""
                                 textViewDns2.text = ""
+                                buttonSettingChange.setText(R.string.set_dhcp)
+                                buttonSettingChange.setOnClickListener {
+                                    wifiManager.disconnect()
+
+                                    val newConf = WifiConfiguration()
+                                    copyWifiConf(conf, newConf)
+                                    callMethod(
+                                        newConf,
+                                        "setIpAssignment",
+                                        arrayOf("android.net.IpConfiguration\$IpAssignment"),
+                                        arrayOf(getEnumValue(
+                                            "android.net.IpConfiguration\$IpAssignment", "DHCP"))
+                                    )
+
+                                    wifiManager.updateNetwork(newConf)
+
+                                    wifiManager.enableNetwork(newConf.networkId, true)
+
+                                    activity?.finish()
+                                }
                             }
                             break
                         }
@@ -109,10 +156,13 @@ class FirstFragment : Fragment() {
             } else {
                 textViewSsid.text = ""
                 textViewIpSetting.text = ""
+                textViewIpAddress.text = ""
                 textViewGateway.text = ""
                 textViewNetworkPrefixLength.text = ""
                 textViewDns1.text = ""
                 textViewDns2.text = ""
+                buttonSettingChange.text = ""
+                buttonSettingChange.setOnClickListener { /* NOP */ }
             }
         }
     }
--- app/src/main/java/com/example/wifi_dhcp_dns_changer/MainActivity.kt
+++ app/src/main/java/com/example/wifi_dhcp_dns_changer/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.wifi_dhcp_dns_changer
 import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -37,10 +36,6 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
